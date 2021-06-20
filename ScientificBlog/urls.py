04c504127d7a09bbb244144c550f2e0923d73e0e
slijirqqq <cult6.cult6.cult6@gmail.com>
2021-06-21 00:32:59 +0400

    Create Scientific Blog

---
+++ ScientificBlog/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Blog.urls'))
+]
+
+# if debug mode is True then project uses debug tool bar and including local media url and root
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls))
+                  ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
