688c633719b2edb4d06b5a257d264c9c5ed633c0
slijirqqq <cult6.cult6.cult6@gmail.com>
2021-06-21 04:29:54 +0400

    Created new view in admin panel for Post model

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,6 +1,7 @@
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Category, Tag, Post
 
@@ -27,6 +28,21 @@ class CategoryAdmin(admin.ModelAdmin):
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     form = PostAdminForm
+    save_as = True
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_filter = ('category', 'tags')
+    readonly_fields = ('views', 'created_at', 'get_photo')
+    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')
+
+    # function for show image in admin panel
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="100">')
+        return '-'
+
+    get_photo.short_description = 'Фото'  # This is description that will show in admin panel
 
 
 @admin.register(Tag)
